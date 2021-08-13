57a09ddee5acd93dfe305e44d13f83e973b69897
srxboys <srxboys@126.com>
2021-08-13 10:49:39 +0800

    [修改] 0.0.3 format-code 2

--- App.js
+++ App.js
@@ -4,15 +4,15 @@
  * author : srxboys
  * @flow  : 用于 静态语法检查
  * ------------------------------
- * 
+ *
  * example
- * 
+ *
 **/
 
 'use strict'
 import React, { Component } from 'react'
 
-import RXIndex   from './app/index'
+import RXIndex from './app/index'
 
 export default class App extends Component {
   render() {
